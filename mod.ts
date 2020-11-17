564fc1d98ebefaa0bf0ce1e5b25eab6602d4cecd
jihuayu <8042833@qq.com>
2020-11-17 18:13:11 +0800

    change env get way

--- mod.ts
+++ mod.ts
@@ -1,23 +1,28 @@
-import {serve} from "https://deno.land/std@0.77.0/http/server.ts";
-import {config} from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
-import {Controller} from "./controller.ts";
+import { serve } from "https://deno.land/std@0.77.0/http/server.ts";
+import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
+import { Controller } from "./controller.ts";
 
-const flag = config({});
+config({ export: true });
 
-const server = serve({hostname: "0.0.0.0", port: +flag['port']});
-const controller = new Controller(flag['remote']);
+const port = Deno.env.get("port") ?? "3000";
+const remote = Deno.env.get("remote");
+if (!remote) {
+  console.error("Need remote");
+  Deno.exit(0);
+}
+const server = serve({ hostname: "0.0.0.0", port: +port });
+const controller = new Controller(remote);
 
-console.log(`Deno function running. Access it at:  http://localhost:${flag['port']}/`);
+console.log(`Deno fass running. Access it at:  http://localhost:${+port}/`);
 
 for await (const request of server) {
-    try{
-        const base = request.headers.get('host') ?? "http://0.0.0.0/";
-        const path = new URL(request.url, base).pathname;
-        controller.call(path, request).then(()=>{
-            console.log(path)
-        })
-    }catch(e){
-        console.error("handle request error!")
-    }
-
+  try {
+    const base = request.headers.get("host") ?? "http://0.0.0.0/";
+    const path = new URL(request.url, base).pathname;
+    controller.call(path, request).then(() => {
+      console.log(path);
+    });
+  } catch (e) {
+    console.error("handle request error!");
+  }
 }
